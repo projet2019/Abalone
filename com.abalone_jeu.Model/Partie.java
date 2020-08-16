5d5862b091fe92dd47d1573d2e4ebf5ee9c0527e
joel yepgang <55999344+projet2019@users.noreply.github.com>
2020-08-16 21:30:22 +0200

    create class Partie

---
+++ com.abalone_jeu.Model/Partie.java
@@ -0,0 +1,23 @@
+package com.abalone_jeu.Model;
+
+import java.awt.Color;
+
+public class Partie {
+	public  Hexagone table;
+	public  Joueur j1;
+	public  Joueur j2;
+	
+	public Partie()
+	{
+		table=new Hexagone(new Coups());
+		j1=new Joueur(Color.BLUE,false,table,table.BouleJoueur1());
+		j2=new Joueur(Color.PINK,false,table,table.bouleJoueur2());
+		j1.setAdversaire(j2);
+		j2.setAdversaire(j1);
+		Joueur [] j=new Joueur[2];
+		j[0]=j1;
+		j[1]=j2;
+		table.setJoueurs(j);
+	}
+
+}
